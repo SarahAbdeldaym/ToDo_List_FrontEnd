8e77c331ad62c74a5c745e63b5fadbda74e64ab0
unknown <sarahabdeldaym@gmil.com>
2022-06-22 21:27:18 +0200

    Add 'Accept: application/json' header to todo requests

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -13,38 +13,33 @@ export class TodoService {
   constructor(private httpClient: HttpClient) { }
 
   getTodos(): Observable<any> {
-    return this.httpClient.get(this.baseUrl, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.get(this.baseUrl, { headers: this.headers() });
   }
 
   createTodo(todo: Todo): Observable<any> {
-    return this.httpClient.post(this.baseUrl, todo, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.post(this.baseUrl, todo, { headers: this.headers() });
   }
 
   updateTodo(todoId: number, request: UpdateTodoRequest): Observable<any> {
-    return this.httpClient.put(this.baseUrl + '/' + todoId, request, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.put(this.baseUrl + '/' + todoId, request, { headers: this.headers() });
   }
 
   markTodoAsDone(todoId: number): Observable<any> {
-    return this.httpClient.post(this.baseUrl + '/' + todoId + '/' + 'mark-as-done', {}, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.post(this.baseUrl + '/' + todoId + '/' + 'mark-as-done', {}, { headers: this.headers() });
   }
 
   reopenTodo(todoId: number): Observable<any> {
-    return this.httpClient.post(this.baseUrl + '/' + todoId + '/' + 'reopen', {}, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.post(this.baseUrl + '/' + todoId + '/' + 'reopen', {}, { headers: this.headers() });
   }
 
   deleteTodo(todoId: number): Observable<any> {
-    return this.httpClient.delete(this.baseUrl + '/' + todoId, { headers: {
-      'Authorization': 'Bearer ' + localStorage.getItem('auth-token')
-    } });
+    return this.httpClient.delete(this.baseUrl + '/' + todoId, { headers: this.headers() });
+  }
+
+  private headers() {
+    return {
+      'Authorization': 'Bearer ' + localStorage.getItem('auth-token'),
+      'Accept': 'application/json'
+    };
   }
  }
